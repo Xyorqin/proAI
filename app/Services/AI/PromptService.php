6997b8ad6f87b91f40a2c82b705dcf3249625a6f
xudoynazarovyorqin <xudoynazarovyorqin@gmail.com>
2025-07-04 05:09:45 +0500

    Done

--- app/Services/AI/PromptService.php
+++ app/Services/AI/PromptService.php
@@ -4,6 +4,8 @@ namespace App\Services\AI;
 
 use App\Models\AI\Prompt;
 use App\Models\Files\File;
+use PhpOffice\PhpWord\IOFactory;
+use Smalot\PdfParser\Parser;
 
 class PromptService
 {
@@ -22,5 +24,36 @@ class PromptService
                 'uploaded_at' => now(),
             ]),
         ]);
+    }
+
+
+
+    public function readFile(int $userId)
+    {
+        $file = File::where('user_id', $userId)
+            ->latest()
+            ->first();
+
+
+        if ($file && strtolower(pathinfo($file->path, PATHINFO_EXTENSION)) === 'pdf') {
+            $parser = new Parser();
+            $pdf = $parser->parseFile(storage_path('app/' . $file->path));
+            $text = $pdf->getText();
+        }
+
+        if ($file && strtolower(pathinfo($file->path, PATHINFO_EXTENSION)) === 'docx') {
+
+            $phpWord = IOFactory::load(storage_path('app/' . $file->path));
+            $text = '';
+            foreach ($phpWord->getSections() as $section) {
+                foreach ($section->getElements() as $element) {
+                    if (method_exists($element, 'getText')) {
+                        $text .= $element->getText() . "\n";
+                    }
+                }
+            }
+        }
+
+        return $text ?? '';
     }
 }
